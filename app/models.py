06c31fd8bf5bfad1473cecfb3232d3bcb92bfd3d
mihamarjanovic <marjanovic.galaxy@gmail.com>
2025-06-15 16:59:43 +0200

    Finished two first points, property retrieval and property search

--- app/models.py
+++ app/models.py
@@ -3,3 +3,46 @@ from . import db
 class Building(db.Model):
     __tablename__ = 'building'
     id = db.Column(db.Integer, primary_key=True)
+    square_footage = db.Column(db.Float)
+    construction_year = db.Column(db.Integer)
+    land_area = db.Column(db.Float)
+    registration = db.Column(db.Boolean)
+    rooms = db.Column(db.Float)
+    bathrooms = db.Column(db.Integer)
+    parking = db.Column(db.Boolean)
+    price = db.Column(db.Integer)
+    estate_type_id = db.Column(db.Integer, db.ForeignKey('estate_type.id'), nullable=False)
+    offer_id = db.Column(db.Integer, db.ForeignKey('offer.id'), nullable=False)
+    city_part_id = db.Column(db.Integer, db.ForeignKey('city_part.id'), nullable=False)
+    estate_type = db.relationship('EstateType')
+    offer = db.relationship('Offer')
+    city_part = db.relationship('CityPart')
+
+class EstateType(db.Model):
+    __tablename__ = 'estate_type'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+class CityPart(db.Model):
+    __tablename__ = 'city_part'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
+    city = db.relationship('City')
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    state_id = db.Column(db.Integer, db.ForeignKey('state.id'), nullable=False)
+    state = db.relationship('State')
+
+class State(db.Model):
+    __tablename__ = 'state'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from . import db
-from .models import Building
+from .models import Building, EstateType, CityPart, City, State
 
 bp = Blueprint('api', __name__)
 
@@ -12,3 +12,58 @@ def index():
 def test_db():
     count = db.session.query(Building).count()
     return f'Total buildings: {count}'
+
+@bp.route('/properties/<int:id>')
+def get_property(id):
+    building = Building.query.get_or_404(id)
+    return {
+        'id': building.id,
+        'square_footage': float(building.square_footage) if building.square_footage is not None else None,
+        'construction_year': building.construction_year,
+        'price': building.price,
+        'rooms': float(building.rooms) if building.rooms is not None else None,
+        'bathrooms': building.bathrooms,
+        'parking': building.parking,
+        'estate_type': building.estate_type.name,
+        'offer': building.offer.name,
+        'city_part': building.city_part.name if building.city_part else None
+    }
+
+@bp.route('/properties')
+def search_properties():
+    query = Building.query
+
+    estate_type = request.args.get('estate_type')
+    if estate_type:
+        query = query.join(EstateType).filter(EstateType.name.ilike(estate_type))
+
+    min_square_footage = request.args.get('min_square_footage', type=float)
+    if min_square_footage:
+        query = query.filter(Building.square_footage >= min_square_footage)
+
+    max_square_footage = request.args.get('max_square_footage', type=float)
+    if max_square_footage:
+        query = query.filter(Building.square_footage <= max_square_footage)
+
+    parking = request.args.get('parking', type=str)
+    if parking and parking.lower() in ['yes', 'no']:
+        query = query.filter(Building.parking == (parking.lower() == 'yes'))
+
+    state = request.args.get('state')
+    if state:
+        query = query.join(CityPart).join(City).join(State).filter(State.name.ilike(state))
+
+    properties = query.all()
+
+    return [{
+        'id': building.id,
+        'square_footage': float(building.square_footage) if building.square_footage is not None else None,
+        'construction_year': building.construction_year,
+        'price': building.price,
+        'rooms': float(building.rooms) if building.rooms is not None else None,
+        'bathrooms': building.bathrooms,
+        'parking': building.parking,
+        'estate_type': building.estate_type.name,
+        'offer': building.offer.name,
+        'city_part': building.city_part.name if building.city_part else None
+    } for building in properties]
