c2dd83754ecc50459284fa9d854144dc06a70d00
mihamarjanovic <marjanovic.galaxy@gmail.com>
2025-06-17 18:06:04 +0200

    Added docker to project

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///real_estate'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://real_estate_user:real_estate_password@localhost/real_estate')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
